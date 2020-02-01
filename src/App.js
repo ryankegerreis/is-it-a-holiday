a89b3b195a77e628db14ab110b85a70b1e0bc381
Ryan Kegerreis <ryankegerreis@icloud.com>
2020-02-01 18:32:52 -0500

    Basic structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react'
+import axios from 'axios'
+
 import './App.css';
+import { API_KEY } from './const'
+import Holiday from './Views/Holiday'
+import NoHoliday from './Views/NoHoliday'
 
-function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+const CALENDAR = `https://calendarific.com/api/v2/holidays?&api_key=${API_KEY}&country=US&year=2019&month=12&day=25`
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      holiday: []
+    }
+  }
+  componentDidMount() {
+    axios.get(CALENDAR).then((res) => {
+      console.log(res.data)
+      if (res.data.response.holidays.length >= 1) {
+        this.setState({
+          holiday: res.data.response.holidays
+        })
+      }
+    }).then((err) => {
+      console.log(err)
+    }).then(console.log("Well that happened"))
+  }
+
+  renderView() {
+    if (this.state.holiday.length >= 1) {
+      return <Holiday holiday={this.state.holiday} />
+    } else {
+      return <NoHoliday />
+    }
+  }
+
+  render() {
+    console.log(this.state.holiday)
+    return (
+      <div>
+        {this.renderView()}
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/Views/NoHoliday.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function NoHoliday() {
+  return (
+    <div>
+      <div className='noholiday-box'>
+        <div className='box'>
+          <h1>Nope today is not a holiday!</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
