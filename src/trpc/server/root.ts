2594a2b2fc80bed0fdccb7c0721e5dd8e903b86d
Ryan Matthew Kegerreis <rkegerreis@centegix.com>
2026-03-12 08:52:08 -0400

    Remove holiday checking functionality, associated tRPC router and UI component, and update the README.

--- src/trpc/server/root.ts
+++ src/trpc/server/root.ts
@@ -1,8 +1,6 @@
 import { createTRPCRouter } from "~/trpc/init";
-import { holidayRouter } from "./routers/holiday";
 
 export const appRouter = createTRPCRouter({
-  holiday: holidayRouter,
 });
 
 export type AppRouter = typeof appRouter;
